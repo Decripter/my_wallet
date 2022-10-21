39b3e1eca7b2e7500d45f7bbf9d0a731409fdaec
Decripter <jeilsonaraujo@icloud.com>
2022-10-21 13:44:10 -0300

    feat:finish usecase_card_number and addd your tests

--- lib/add_wallet/usecases/card_number_usecase.dart
+++ lib/add_wallet/usecases/card_number_usecase.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: comment_references
+
 import 'package:my_wallet/add_wallet/value_objects/card_number_value_object.dart.dart';
 
 /// This will suport all validations and methods that a [CardNumber] needs
@@ -13,7 +15,14 @@ class CardNumerUsecase {
   bool validateCard(CardNumber card) {
     final isCorrectlyLength = checkCorrectlyLength(card);
     if (isCorrectlyLength) {
-      return true;
+      final lastDigit = takeLastDigit(card);
+      var result = 0;
+      result = getSumOfNumbersOfCard(card);
+      result = result % 10;
+      if (result != 0) {
+        result = 10 - result;
+      }
+      return result == lastDigit;
     } else {
       return isCorrectlyLength;
     }
@@ -29,10 +38,60 @@ class CardNumerUsecase {
     }
   }
 
-  /// This will teka a [CardNumber] and return the last digit
-  String takeLastDigit(CardNumber card) {
+  /// This will take a [CardNumber] and return the last digit
+  int takeLastDigit(CardNumber card) {
     final cardNumber = removeMask(card);
     final lastDigit = cardNumber[cardNumber.length - 1];
-    return lastDigit;
+    return int.parse(lastDigit);
   }
+
+  /// This will make the multiplication process
+  int multiplyDigits({required int digit, required int multiply}) {
+    return digit * multiply;
+  }
+
+  /// This will take a [int] number and sum if it has more than one digit
+  int getSumOfNumber(int number) {
+    if (number.toString().length > 1) {
+      var numberCalculated = 0;
+      final numberAsString = number.toString();
+      for (var digit = 0; digit < numberAsString.length; digit++) {
+        numberCalculated = numberCalculated + int.parse(numberAsString[digit]);
+      }
+      return numberCalculated;
+    } else {
+      return number;
+    }
+  }
+
+  /// This will througth of digits and get the sum of all these
+  int getSumOfNumbersOfCard(CardNumber card) {
+    final numbers = removeMask(card);
+    var result = 0;
+    for (var digit = 0; digit < numbers.length; digit++) {
+      final value = int.parse(numbers[digit]);
+      final digitValue = getSumOfNumber(
+        multiplyDigits(
+          digit: value,
+          multiply: getMultiplyProperty(digit),
+        ),
+      );
+      result = result + digitValue;
+    }
+
+    return result;
+  }
+
+  /// Take a digit and return the multiply property correctly
+  /// 1 for [digit.isEven],
+  /// and 0 for [digit.isOdd]
+  int getMultiplyProperty(int digit) {
+    if (digit.isEven) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+
+  ///
 }
--- test/add_wallet/usecases/card_number_usecase_test.dart
+++ test/add_wallet/usecases/card_number_usecase_test.dart
@@ -47,29 +47,145 @@ void main() {
     });
   });
 
-  group('GET_LAST_DIGIT -> ', () {
+  group('TAKE_LAST_DIGIT -> ', () {
     test('should return 4 on the last digit', () {
       final cardEndWithDigit4 = CardNumber(value: '1111 2222 3333 4444');
       final result = usecase.takeLastDigit(cardEndWithDigit4);
-      expect(result, '4');
+      expect(result, 4);
     });
     test('should return 4 on the last digit even dirty', () {
       final cardEndWithDigit4Dirty = CardNumber(value: '1111 22292 3333s4444a');
       final result = usecase.takeLastDigit(cardEndWithDigit4Dirty);
-      expect(result, '4');
+      expect(result, 4);
     });
     test('should return 0 on the last digit', () {
       final cardEndWithDigit0 = CardNumber(value: '1111 2232 4431 0110');
       final result = usecase.takeLastDigit(cardEndWithDigit0);
-      expect(result, '0');
+      expect(result, 0);
     });
     test('should return 0 on the last digit even dirty', () {
       final cardEndWithDigit0Dirty = CardNumber(value: '1111 2232w4431 0110ww');
       final result = usecase.takeLastDigit(cardEndWithDigit0Dirty);
-      expect(result, '0');
+      expect(result, 0);
     });
   });
 
+  group('MULTIPLY_DIGITS -> ', () {
+    test('should return 8 to the digit 4 * 2', () {
+      final result = usecase.multiplyDigits(digit: 4, multiply: 2);
+      expect(result, 8);
+    });
+    test('should return 4 to the digit 4 * 1', () {
+      final result = usecase.multiplyDigits(digit: 4, multiply: 1);
+      expect(result, 4);
+    });
+    test('should return 0 to the digit 0 * 2', () {
+      final result = usecase.multiplyDigits(digit: 0, multiply: 2);
+      expect(result, 0);
+    });
+    test('should return 0 to the digit 8 * 1', () {
+      final result = usecase.multiplyDigits(digit: 0, multiply: 1);
+      expect(result, 0);
+    });
+  });
+
+  group('SUM_OF_NUMBERS -> ', () {
+    test('should return 6 to the number 15', () {
+      final result = usecase.getSumOfNumber(15);
+      expect(result, 6);
+    });
+    test('should return 1 to the number 10', () {
+      final result = usecase.getSumOfNumber(10);
+      expect(result, 1);
+    });
+    test('should return 1 to the number 1', () {
+      final result = usecase.getSumOfNumber(1);
+      expect(result, 1);
+    });
+    test('should return 10 to the number 19', () {
+      final result = usecase.getSumOfNumber(19);
+      expect(result, 10);
+    });
+    test('should return 3 to the number 111', () {
+      final result = usecase.getSumOfNumber(111);
+      expect(result, 3);
+    });
+  });
+
+  group('GET_MULTIPLY_PROPERTY -> ', () {
+    test('should return 2 to the number 10 [even]', () {
+      final result = usecase.getMultiplyProperty(10);
+      expect(result, 2);
+    });
+    test('should return 2 to the number 18 [even]', () {
+      final result = usecase.getMultiplyProperty(18);
+      expect(result, 2);
+    });
+    test('should return 1 to the number 00 [even]', () {
+      final result = usecase.getMultiplyProperty(0);
+      expect(result, 2);
+    });
+
+    test('should return 1 to the number 11 [odd]', () {
+      final result = usecase.getMultiplyProperty(1);
+      expect(result, 1);
+    });
+    test('should return 1 to the number 05 [odd]', () {
+      final result = usecase.getMultiplyProperty(5);
+      expect(result, 1);
+    });
+  });
+
+  group('SUM_OF_NUMBERS_CARD -> ', () {
+    test('should return 24 to the card 1111 1111 1111 1111', () {
+      final card = CardNumber(value: '1111 1111 1111 1111');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 24);
+    });
+    test('should return 16 to the card 1010 1010 1010 1010', () {
+      final card = CardNumber(value: '1010 1010 1010 1010');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 16);
+    });
+    test('should return 8 to the card 0101 0101 0101 0101', () {
+      final card = CardNumber(value: '0101 0101 0101 0101');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 8);
+    });
+    test('should return 0 to the card 0000 0000 0000 0000', () {
+      final card = CardNumber(value: '0000 0000 0000 0000');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 0);
+    });
+    test('should return 1 to the card 0000 0000 0000 0001', () {
+      final card = CardNumber(value: '0000 0000 0000 0001');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 1);
+    });
+    test('should return 2 to the card 0000 0000 0000 0010', () {
+      final card = CardNumber(value: '0000 0000 0000 0010');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 2);
+    });
+    test('should return 80 to the card 4916 6418 5936 9080', () {
+      final card = CardNumber(value: '4916 6418 5936 9080');
+      final result = usecase.getSumOfNumbersOfCard(card);
+      expect(result, 80);
+    });
+  });
+
+  group('VALIDATE_CARD -> ', () {
+    test('should return false to the card 5419 8250 0346 1210', () {
+      final card = CardNumber(value: '5419 8250 0346 1210');
+      final result = usecase.validateCard(card);
+      expect(result, false);
+    });
+    test('should return true to the card 4916 6418 5936 9080', () {
+      final card = CardNumber(value: '4916 6418 5936 9080');
+      final result = usecase.validateCard(card);
+      expect(result, true);
+    });
+  });
   // END OF TEST SUIT
 }
 
@@ -85,7 +201,8 @@ void main() {
 // 2. Multiplique a numeração do cartão, número por número sendo a 
 //    coluna ímpar por 1 e a coluna par por 2, levando em conta que 
 //    a primeira coluna inicia-se por 0 (zero). 
-//    Se o valor da multiplicação tiver dois dígitos, separe e some cada unidade. 
+//    Se o valor da multiplicação tiver dois dígitos, 
+//    separe e some cada unidade. 
 //    Em seguida, some todos os valores.
 
 // 3. Com o resultado anterior, faça uma divisão por 10 para obter o
@@ -102,34 +219,3 @@ void main() {
 
 // Cartão inválido: 5419 8250 0346 1210.
 // Cartão válido:   4916 6418 5936 9080.
-
-//   par => * 2
-// impar => * 1
-
-// 1 -> [0]
-
-// 2 * [ 0 2 6 3 0 2 9 4 ] => 0 4 1 2 6 0 4 1 8 8 =>  34
-// 1 * [ 1 1 4 0 5 8 1 5 ] => 1 1 4 0 5 8 1 5     =>  25
-//                                                    59
-
-//2 -> [59]
-
-
-// 3 -> 9
-
-// 10 - 9 = 1
-// 4 -> 1
-
-// 5 -> 1 != 0
-
-
-
-
-//2 * [ 6 9 8 5 6 9    =>  32
-//1 * [8 9 3 5 1 6 1 4 =>  38 
-//                         70
-//
-//  70%10 => 0
-//
-//  0 == 0 
-//
