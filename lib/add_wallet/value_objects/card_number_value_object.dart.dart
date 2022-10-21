590ec654e38fc173de072922620f7cf3099004b4
Decripter <jeilsonaraujo@icloud.com>
2022-10-20 22:43:07 -0300

    feat:add removeMask on card_usecase

--- lib/add_wallet/value_objects/card_number_value_object.dart.dart
+++ lib/add_wallet/value_objects/card_number_value_object.dart.dart
@@ -1,6 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+/// represents a number on a card store
 class CardNumber {
+  /// its a strting becouse its can contains masks that
+  /// will be added or removed on usecase layer
   String value;
+
+  /// it's define te constructor method
   CardNumber({
     required this.value,
   });
+
+  /// this return the value instead <InstanceOfCardNumber>
+  @override
+  String toString() {
+    return value;
+  }
+
+  /// allows mutability on class
+  CardNumber copyWith({
+    String? value,
+  }) {
+    return CardNumber(
+      value: value ?? this.value,
+    );
+  }
 }
